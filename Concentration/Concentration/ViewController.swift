3daae04caa145dadc3f453c3af994bd5e4640d82
redevaaa <redevaaa@gmail.com>
2020-12-04 22:06:14 +0800

    simplify the code

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -16,12 +16,26 @@ class ViewController: UIViewController {
     }
     
     private(set) var flipCount = 0 {
-        didSet{
-            flipCountLabel.text = "Flips: \(flipCount)"
+        didSet {
+            updateFlipCountLabel()
         }
     }
+    
+    func updateFlipCountLabel(){
+        let attributes: [NSAttributedString.Key: Any] = [
+            .strokeColor : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)  ,
+            .strokeWidth : 2,
+            
+        ]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
 
-    @IBOutlet private weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet{
+            updateFlipCountLabel()
+        }
+    }
     
     @IBOutlet private var cardButtons: [UIButton]!
     
@@ -49,14 +63,16 @@ class ViewController: UIViewController {
         }
     }
     
-    private var emojiChoices = ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎"]
-    private var emoji = [Int: String]()
+//    private var emojiChoices = ["🦇","😱","🙀","😈","🎃","👻","🍭","🍬","🍎"]
+    var emojiChoices = "🦇😱🙀😈🎃👻🍭🍬🍎"
+    private var emoji = [Card: String]()
     
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0{
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.randomNumber)
+        if emoji[card] == nil, emojiChoices.count > 0{
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.randomNumber)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
 
 }
